77161d86225559a01ff9fc7b5ac634dbd9bc62c3
romanrosh <45785380+romanrosh@users.noreply.github.com>
2018-12-26 13:32:00 +0200

    Add files via upload

---
+++ test remove background.py
@@ -0,0 +1,36 @@
+# Load image
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Load image
+image_bgr = cv2.imread('snatch.jpg')
+# Convert to RGB
+image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+# Rectange values: start x, start y, width, height
+rectangle = (216, 1, 500, 350)
+# Create initial mask
+mask = np.zeros(image_rgb.shape[:2], np.uint8)
+
+# Create temporary arrays used by grabCut
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
+
+# Run grabCut
+cv2.grabCut(image_rgb, # Our image
+            mask, # The Mask
+            rectangle, # Our rectangle
+            bgdModel, # Temporary array for background
+            fgdModel, # Temporary array for background
+            7, # Number of iterations
+            cv2.GC_INIT_WITH_RECT) # Initiative using our rectangle
+
+# Create mask where sure and likely backgrounds set to 0, otherwise 1
+mask_2 = np.where((mask==2) | (mask==0), 0, 1).astype('uint8')
+
+# Multiply image with new mask to subtract background
+image_rgb_nobg = image_rgb * mask_2[:, :, np.newaxis]
+
+# Show image
+plt.imshow(image_rgb_nobg), plt.axis("off")
+plt.show()
---
+++ test video read from file.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('drop.avi')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    print(cap.read())
+
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
