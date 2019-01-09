e3159ade67e9eae53150c8f3338ef042eede7c5d
roeeorland <roee.orland@gmail.com>
2019-01-09 10:16:50 +0200

    added Ro'ee's python code

---
+++ resize.py
@@ -0,0 +1,13 @@
+from PIL import Image
+import os
+
+from resizeimage import resizeimage
+
+for filename in os.listdir('/home/roee/Pictures'):
+    if filename.endswith(".jpg") : 
+    	with open(filename, 'r+b') as f:
+    		with Image.open(f) as image:
+        		cover = resizeimage.resize_cover(image, [150, 150])
+        		cover.save(f'resized_{filename}', image.format)
+        # print(os.path.join(directory, filename))
+       
