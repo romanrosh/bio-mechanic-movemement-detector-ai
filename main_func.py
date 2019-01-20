6ab71bc15f274750c2e332862348e6689672aee7
Roman Rosh <romanrosh@gmail.com>
2019-01-20 23:24:52 +0200

    Minor fix

--- main_func.py
+++ main_func.py
@@ -1,19 +1,19 @@
-
 from tkinter import *
-
 
 
 def button1(param):
     print('inside button1', param)
 
+
 def button2(param):
     print('inside button2', param)
 
-def button2(param):
+
+def button3(param):
     print('inside button3', param)
 
-def main():
 
+def main():
     input_width = 150
     root = Tk()
     root.resizable(width=FALSE, height=FALSE)
@@ -26,7 +26,7 @@ def main():
     button1inputtext.place(x=10, y=70, width=250, height=25)
     button1input = Entry(root)
     button1input.place(x=10, y=100, width=70 + (input_width + 20) * 2, height=25)
-    goButton1 = Button(root, text='button1',command=lambda: button1(button1input.get()))
+    goButton1 = Button(root, text='button1', command=lambda: button1(button1input.get()))
     goButton1.config(relief=RAISED)
     goButton1.place(x=450, y=100, width=100, height=25)
 
@@ -34,7 +34,7 @@ def main():
     button2inputtext.place(x=10, y=120, width=250, height=25)
     button2input = Entry(root)
     button2input.place(x=10, y=150, width=70 + (input_width + 20) * 2, height=25)
-    goButton2 = Button(root, text='button2',command=lambda: button2(button2input.get()))
+    goButton2 = Button(root, text='button2', command=lambda: button2(button2input.get()))
     goButton2.config(relief=RAISED)
     goButton2.place(x=450, y=150, width=100, height=25)
 
@@ -42,11 +42,11 @@ def main():
     button3inputtext.place(x=10, y=170, width=250, height=25)
     button3input = Entry(root)
     button3input.place(x=10, y=200, width=70 + (input_width + 20) * 2, height=25)
-    goButton3 = Button(root, text='button3',command=lambda: button3(button3input.get()))
+    goButton3 = Button(root, text='button3', command=lambda: button3(button3input.get()))
     goButton3.config(relief=RAISED)
     goButton3.place(x=450, y=200, width=100, height=25)
 
-    ExitButton = Button(root, text='Exit',command=exit)
+    ExitButton = Button(root, text='Exit', command=exit)
     ExitButton.config(relief=RAISED)
     ExitButton.place(x=500, y=400, width=100, height=25)
 
