93822ce6123b8700a20e774c90c9639a5cc1ff36
Roman Rosh <romanrosh@gmail.com>
2019-02-11 15:43:03 +0200

    Adding Body25

--- OpenPoseVideo.py
+++ OpenPoseVideo.py
@@ -8,40 +8,44 @@ if MODE is "COCO":
     protoFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/coco/pose_deploy_linevec.prototxt"
     weightsFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/coco/pose_iter_440000.caffemodel"
     nPoints = 18
-    POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
+    POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+                  [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
 
-elif MODE is "MPI" :
+elif MODE is "MPI":
     protoFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
     weightsFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/mpi/pose_iter_160000.caffemodel"
     nPoints = 15
-    POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
+    POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14], [14, 8], [8, 9], [9, 10], [14, 11],
+                  [11, 12], [12, 13]]
 
-elif MODE is "BODY25" :
+elif MODE is "BODY25":
     protoFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/body_25/pose_deploy.prototxt"
     weightsFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/body_25/pose_iter_584000.caffemodel"
     nPoints = 25
-    POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17],
-                   [10,11],[8,12],[12,13],[13,14],[1,0],[0,15],[0,16],[16,18],[2,17],[5,18],[14,19],[19,20],[14,21],[11,22],[22,23],[11,24]]
-
-
-
+    POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+                  [12, 13], [0, 14], [0, 15], [14, 16], [15, 17],
+                  [10, 11], [8, 12], [12, 13], [13, 14], [1, 0], [0, 15], [0, 16], [16, 18], [2, 17], [5, 18], [14, 19],
+                  [19, 20], [14, 21], [11, 22], [22, 23], [11, 24]]
 
 inWidth = 368
 inHeight = 368
 threshold = 0.1
 
-
-input_source = "Roman10.MOV"
+input_source = "1.mp4"
 cap = cv2.VideoCapture(input_source)
 hasFrame, frame = cap.read()
 
-vid_writer = cv2.VideoWriter('/destination/output.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (frame.shape[1],frame.shape[0]))
+vid_writer = cv2.VideoWriter('./destination/output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10,
+                             (frame.shape[1], frame.shape[0]))
 
 net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
-
+counter = 0
 while cv2.waitKey(1) < 0:
     t = time.time()
     hasFrame, frame = cap.read()
+    counter += 1
+    if np.mod(counter, 5) != 0:
+        continue
     frameCopy = np.copy(frame)
     if not hasFrame:
         cv2.waitKey()
@@ -51,7 +55,7 @@ while cv2.waitKey(1) < 0:
     frameHeight = frame.shape[0]
 
     inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
-                              (0, 0, 0), swapRB=False, crop=False)
+                                    (0, 0, 0), swapRB=False, crop=False)
     net.setInput(inpBlob)
     output = net.forward()
 
@@ -66,18 +70,19 @@ while cv2.waitKey(1) < 0:
 
         # Find global maxima of the probMap.
         minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-        
+
         # Scale the point to fit on the original image
         x = (frameWidth * point[0]) / W
         y = (frameHeight * point[1]) / H
 
-        if prob > threshold : 
+        if prob > threshold:
             cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+            cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                        lineType=cv2.LINE_AA)
 
             # Add the point to the list if the probability is greater than the threshold
             points.append((int(x), int(y)))
-        else :
+        else:
             points.append(None)
 
     # Draw Skeleton
@@ -89,8 +94,9 @@ while cv2.waitKey(1) < 0:
             cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
             cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
             cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
-
-    cv2.putText(frame, "time taken = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8, (255, 50, 0), 2, lineType=cv2.LINE_AA)
+        print(points)
+    # cv2.putText(frame, "time taken = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8,
+    #             (255, 50, 0), 2, lineType=cv2.LINE_AA)
     # cv2.putText(frame, "OpenPose using OpenCV", (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 50, 0), 2, lineType=cv2.LINE_AA)
     # cv2.imshow('Output-Keypoints', frameCopy)
     cv2.imshow('Output-Skeleton', frame)
