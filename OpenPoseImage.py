4dc71a2c819cb13a362895543dd2562fef72e93e
Roman Rosh <romanrosh@gmail.com>
2019-02-11 15:42:47 +0200

    Adding Body25

--- OpenPoseImage.py
+++ OpenPoseImage.py
@@ -8,24 +8,27 @@ if MODE is "COCO":
     protoFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/coco/pose_deploy_linevec.prototxt"
     weightsFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/coco/pose_iter_440000.caffemodel"
     nPoints = 18
-    POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17]]
+    POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+                  [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
 
-elif MODE is "MPI" :
+elif MODE is "MPI":
     protoFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
     weightsFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/mpi/pose_iter_160000.caffemodel"
     nPoints = 15
-    POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
+    POSE_PAIRS = [[0, 1], [1, 2], [2, 3], [3, 4], [1, 5], [5, 6], [6, 7], [1, 14], [14, 8], [8, 9], [9, 10], [14, 11],
+                  [11, 12], [12, 13]]
 
-elif MODE is "BODY25" :
+elif MODE is "BODY25":
     protoFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/body_25/pose_deploy.prototxt"
     weightsFile = "C:/Users/romanrosh/openpose-1.4.0-win64-gpu-binaries/models/pose/body_25/pose_iter_584000.caffemodel"
     nPoints = 25
-    POSE_PAIRS = [ [1,0],[1,2],[1,5],[2,3],[3,4],[5,6],[6,7],[1,8],[8,9],[9,10],[1,11],[11,12],[12,13],[0,14],[0,15],[14,16],[15,17],
-                   [10,11],[8,12],[12,13],[13,14],[1,0],[0,15],[0,16],[16,18],[2,17],[5,18],[14,19],[19,20],[14,21],[11,22],[22,23],[11,24]]
+    POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+                  [12, 13], [0, 14], [0, 15], [14, 16], [15, 17],
+                  [10, 11], [8, 12], [12, 13], [13, 14], [1, 0], [0, 15], [0, 16], [16, 18], [2, 17], [5, 18], [14, 19],
+                  [19, 20], [14, 21], [11, 22], [22, 23], [11, 24]]
 
-
-frame = cv2.imread("1.jpg")
-frame=cv2.resize(frame,dsize=(500,800))
+frame = cv2.imread("2.jpg")
+frame = cv2.resize(frame, dsize=(1000, 800))
 frameCopy = np.copy(frame)
 frameWidth = frame.shape[1]
 frameHeight = frame.shape[0]
@@ -38,7 +41,7 @@ t = time.time()
 inWidth = 368
 inHeight = 368
 inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight),
-                          (0, 0, 0), swapRB=False, crop=False)
+                                (0, 0, 0), swapRB=False, crop=False)
 
 net.setInput(inpBlob)
 
@@ -57,18 +60,19 @@ for i in range(nPoints):
 
     # Find global maxima of the probMap.
     minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
-    
+
     # Scale the point to fit on the original image
     x = (frameWidth * point[0]) / W
     y = (frameHeight * point[1]) / H
 
-    if prob > threshold : 
+    if prob > threshold:
         cv2.circle(frameCopy, (int(x), int(y)), 8, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
-        cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, lineType=cv2.LINE_AA)
+        cv2.putText(frameCopy, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                    lineType=cv2.LINE_AA)
 
         # Add the point to the list if the probability is greater than the threshold
         points.append((int(x), int(y)))
-    else :
+    else:
         points.append(None)
 
 # Draw Skeleton
@@ -80,15 +84,12 @@ for pair in POSE_PAIRS:
         cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
         cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
 
-
 cv2.imshow('Output-Keypoints', frameCopy)
 cv2.imshow('Output-Skeleton', frame)
 
-
-cv2.imwrite('Output-Keypoints.jpg', frameCopy)
-cv2.imwrite('Output-Skeleton.jpg', frame)
+cv2.imwrite('./destination/Output-Keypoints.jpg', frameCopy)
+cv2.imwrite('./destination/Output-Skeleton.jpg', frame)
 
 print("Total time taken : {:.3f}".format(time.time() - t))
 
 cv2.waitKey(0)
-
