d77495c5b419bde11bd60d7ac703a21089470057
Tom Cohen <tomcohen0079@gmail.com>
2019-02-14 16:51:19 +0200

    working with annotation

--- TOM/create_dataframe.py
+++ TOM/create_dataframe.py
@@ -7,32 +7,8 @@ CURRENT_DIR = os.getcwd()
 VIDEO_DIR = os.path.join(CURRENT_DIR, './videos/')
 TARGET_DIR = os.path.join(CURRENT_DIR, './videos_frames/')
 TARGET_CSV = os.path.join(CURRENT_DIR, './csv/')
-TRUTH = 0
-BODY_25_COLUMNS = ["0-XNose", "0-YNose",
-                   "1-XNeck", "1-YNeck",
-                   "2-XRShoulder", "2-YRShoulder",
-                   "3-XRElbow", "3-YRElbow",
-                   "4-XRWrist", "4-YRWrist",
-                   "5-XLShoulder", "5-YLShoulder",
-                   "6-XLElbow", "6-YLElbow",
-                   "7-XLWrist", "7-YLWrist",
-                   "8-XMidHip", "8-YMidHip",
-                   "9-XRHip", "9-YRHip",
-                   "10-XRKnee", "10-YRKnee",
-                   "11-XRAnkle", "11-YRAnkle",
-                   "12-XLHip", "12-YLHip",
-                   "13-XLKnee", "13-YLKnee",
-                   "14-XLAnkle", "14-YLAnkle",
-                   "15-XREye", "15-YREye",
-                   "16-XLEye", "16-YLEye",
-                   "17-XREar", "17-YREar",
-                   "18-XLEar", "18-YLEar",
-                   "19-XLBigToe", "19-YLBigToe",
-                   "20-XLSmallToe", "20-YLSmallToe",
-                   "21-XLHeel", "21-YLHeel",
-                   "22-XRBigToe", "22-YRBigToe",
-                   "23-XRSmallToe", "23-YRSmallToe",
-                   "24-XRHeel", "24-YRHeel"]
+TRUTH = 1
+
 
 
 def rotate_image(img_file, degrees):
